25a0538bcc4be9d1f00baf9554185f50b2853b44
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2022-12-05 16:54:22 +0100

    [feat] Set the call to get the details of a phone

---
+++ src/client/apiCalls/fetchPhoneDetails.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const getPhoneDetails = async (url) => {
+    const request = await axios({
+        method: "GET",
+        url: url,
+        headers: {
+            "content-type": "application/json"
+        }
+    });
+
+    return request;
+}
+
+export {getPhoneDetails}
