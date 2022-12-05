a9b7321bcc2a22a87e90861d7a0be452f0ffadb6
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2022-12-05 16:56:58 +0100

    [feat] Set the functions
    - Get the list of phones in a dropdown menu
    - Show the phones' brand
    - Get all the phones from a brand

--- src/client/App.js
+++ src/client/App.js
@@ -1,9 +1,38 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useRef} from "react";
 
-import {getAllBrands} from "./api-calls/fetch-all-brands";
+import Modal from "./components/modal/modal";
 
-function App() {
+import {getAllBrands} from "./apiCalls/fetchAllBrands";
+import {getPhonesFromBrand} from "./apiCalls/fetchPhonesFromBrand";
+import {getPhoneDetails} from "./apiCalls/fetchPhoneDetails";
+
+const App = () => {
 	const [brands, setBrands] = useState([]);
+	const [brandTitle, setBrandTitle] = useState(null);
+	const [phones, setPhones] = useState(null);
+	const [isModalOpen, setIsModalOpen] = useState(false);
+	const dropDown = useRef(null);
+
+	const getOption = () => {
+		let selected = dropDown.current.value;
+
+		getPhonesFromBrand(selected)
+		.then(result => {
+			setBrandTitle(result.data.data.title);
+			setPhones(result.data.data.phones);
+		});
+	}
+
+	const acquirePhoneDetails = (url) => {
+		return () => {
+			getPhoneDetails(url)
+			.then(result => console.log(result.data.data));
+		}
+	}
+
+	const toggleModal = () => {
+		setIsModalOpen({isModalOpen: !isModalOpen});
+	}
 
 	useEffect(() => {
 		getAllBrands()
@@ -13,15 +42,41 @@ function App() {
 	}, []);
 
 	return (
-		<div className="App">
-			{brands.length > 0
-			? <ul>
-				{brands.map(item => 
-					<li key={item.brand_id}>{item.brand_name}</li>
-				)}
-			</ul>
-			: <p>{"Loading"}</p>
-			}
+		<div className={"App"}>
+			<main>
+				<div>
+					<label htmlFor={"brandSelection"}>{"Select a brand"}</label>
+					<select name={"brands"} id={"brandSelection"} ref={dropDown}>
+						<option value={""}>{"Please choose an option"}</option>
+						{brands.length && brands.map(item => 
+							<option value={item.brand_slug} key={item.brand_id}>{item.brand_name}</option>
+						)}
+					</select>
+					<button onClick={getOption}>{"Validate"}</button>
+				</div>
+
+				<div>
+					{brandTitle
+						? <h2>{brandTitle}</h2>
+						: null
+					}
+				</div>
+
+				<div>
+					{phones
+						? phones.map(phone =>
+							<section onClick={toggleModal} key={phone.slug}>
+								<div>
+									<img src={phone.image} alt={phone.brand + phone.phone_name + " image"} />
+								</div>
+								<h3>{phone.brand + phone.phone_name}</h3>
+							</section>
+						)
+						: null
+					}
+					<Modal />
+				</div>
+			</main>
 		</div>
 	);
 }
