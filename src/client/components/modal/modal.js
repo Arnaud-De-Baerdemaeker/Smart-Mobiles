a18a2c47cf8fd9351baecbbc095ee8ecb2d56faa
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2022-12-06 19:08:54 +0100

    [refactor] Move the content in the phone details component

--- src/client/components/modal/modal.js
+++ src/client/components/modal/modal.js
@@ -1,43 +1,9 @@
 import React from "react";
 
-const Modal = ({phoneDetails}) => {
+const Modal = () => {
 	return(
 		<section>
-			{phoneDetails
-				? <>
-					<h2>{`${phoneDetails.brand} ${phoneDetails.phone_name}`}</h2>
-					<div>
-						<div>
-							{phoneDetails.thumbnail
-								? <img src={phoneDetails.thumbnail} alt={`${phoneDetails.brand} ${phoneDetails.phone_name}`} />
-								: null
-							}
-						</div>
-						<div>
-							<dl>
-								<dt>{"Brand"}</dt>
-								<dd>{phoneDetails.brand}</dd>
-
-								<dt>{"Phone Name"}</dt>
-								<dd>{phoneDetails.phone_name}</dd>
-
-								<dt>{"Dimensions"}</dt>
-								<dd>{phoneDetails.dimension}</dd>
-
-								<dt>{"OS"}</dt>
-								<dd>{phoneDetails.os}</dd>
-
-								<dt>{"Storage"}</dt>
-								<dd>{phoneDetails.storage}</dd>
-
-								<dt>{"Release date"}</dt>
-								<dd>{phoneDetails.release_date}</dd>
-							</dl>
-						</div>
-					</div>
-				</>
-				: null
-			}
+			
 		</section>
 	);
 }
