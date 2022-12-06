23c6d19f80831a362c738e96f86749bcd2fd694c
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2022-12-06 12:30:17 +0100

    [add] Populate modal with a first bunch of details data from props

--- src/client/components/modal/modal.js
+++ src/client/components/modal/modal.js
@@ -1,12 +1,45 @@
 import React from "react";
 
-const Modal = () => {
-    return(
-        <section>
-            <h2></h2>
-            <div></div>
-        </section>
-    );
+const Modal = ({phoneDetails}) => {
+	return(
+		<section>
+			{phoneDetails
+				? <>
+					<h2>{`${phoneDetails.brand} ${phoneDetails.phone_name}`}</h2>
+					<div>
+						<div>
+							{phoneDetails.thumbnail
+								? <img src={phoneDetails.thumbnail} alt={`${phoneDetails.brand} ${phoneDetails.phone_name}`} />
+								: null
+							}
+						</div>
+						<div>
+							<dl>
+								<dt>{"Brand"}</dt>
+								<dd>{phoneDetails.brand}</dd>
+
+								<dt>{"Phone Name"}</dt>
+								<dd>{phoneDetails.phone_name}</dd>
+
+								<dt>{"Dimensions"}</dt>
+								<dd>{phoneDetails.dimension}</dd>
+
+								<dt>{"OS"}</dt>
+								<dd>{phoneDetails.os}</dd>
+
+								<dt>{"Storage"}</dt>
+								<dd>{phoneDetails.storage}</dd>
+
+								<dt>{"Release date"}</dt>
+								<dd>{phoneDetails.release_date}</dd>
+							</dl>
+						</div>
+					</div>
+				</>
+				: null
+			}
+		</section>
+	);
 }
 
 export default Modal;
