a77607eb9269a4892f462227bc0c228b2d47ef4a
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2022-12-22 15:30:23 +0100

    [add] Call the Header and Footer components

--- src/client/components/phoneDetails/phoneDetails.js
+++ src/client/components/phoneDetails/phoneDetails.js
@@ -1,73 +1,79 @@
 import React, {useState} from "react";
 
+import Header from "../header/header";
 import Modal from "../modal/modal";
+import Footer from "../footer/footer";
 
 const PhoneDetails = ({phoneDetails}) => {
 	const [modalState, setModalState] = useState(false);
 
 	return(
-		<main>
-			{phoneDetails
-				? <>
-					<h2>{`${phoneDetails.brand} ${phoneDetails.phone_name}`}</h2>
-					<div>
-						<div onClick={() => setModalState(!modalState)}>
-							<img
-								src={phoneDetails.thumbnail}
-								alt={`${phoneDetails.brand} ${phoneDetails.phone_name}`}
-							/>
+		<>
+			<Header />
+			<main>
+				{phoneDetails
+					? <>
+						<h2>{`${phoneDetails.brand} ${phoneDetails.phone_name}`}</h2>
+						<div>
+							<div onClick={() => setModalState(!modalState)}>
+								<img
+									src={phoneDetails.thumbnail}
+									alt={`${phoneDetails.brand} ${phoneDetails.phone_name}`}
+								/>
+							</div>
+							<div>
+								<dl>
+									<dt>{"Brand"}</dt>
+									<dd>{phoneDetails.brand}</dd>
+
+									<dt>{"Phone name"}</dt>
+									<dd>{phoneDetails.phone_name}</dd>
+
+									<dt>{"Dimensions"}</dt>
+									<dd>{phoneDetails.dimension}</dd>
+
+									<dt>{"OS"}</dt>
+									<dd>{phoneDetails.os}</dd>
+
+									<dt>{"Storage"}</dt>
+									<dd>{phoneDetails.storage}</dd>
+
+									<dt>{"Release date"}</dt>
+									<dd>{phoneDetails.release_date}</dd>
+								</dl>
+							</div>
 						</div>
 						<div>
-							<dl>
-								<dt>{"Brand"}</dt>
-								<dd>{phoneDetails.brand}</dd>
-
-								<dt>{"Phone name"}</dt>
-								<dd>{phoneDetails.phone_name}</dd>
-
-								<dt>{"Dimensions"}</dt>
-								<dd>{phoneDetails.dimension}</dd>
-
-								<dt>{"OS"}</dt>
-								<dd>{phoneDetails.os}</dd>
-
-								<dt>{"Storage"}</dt>
-								<dd>{phoneDetails.storage}</dd>
-
-								<dt>{"Release date"}</dt>
-								<dd>{phoneDetails.release_date}</dd>
-							</dl>
+							{phoneDetails.specifications.map(specification =>
+								<section key={specification.title}>
+									<h3>{specification.title}</h3>
+									<dl>
+										{specification.specs.map(spec =>
+											<>
+												<dt>{spec.key}</dt>
+												{spec.val.map(value =>
+													<dd>{value}</dd>
+												)}
+											</>
+										)}
+									</dl>
+								</section>
+							)}
 						</div>
-					</div>
-					<div>
-						{phoneDetails.specifications.map(specification =>
-							<section key={specification.title}>
-								<h3>{specification.title}</h3>
-								<dl>
-									{specification.specs.map(spec =>
-										<>
-											<dt>{spec.key}</dt>
-											{spec.val.map(value =>
-												<dd>{value}</dd>
-											)}
-										</>
-									)}
-								</dl>
-							</section>
-						)}
-					</div>
-					{modalState
-						? <Modal
-							modalState={modalState}
-							images={phoneDetails.phone_images}
-							name={`${phoneDetails.brand} ${phoneDetails.phone_name}`}
-						/>
-						: null
-					}
-				</>
-				: <p>{"Loading"}</p>
-			}
-		</main>
+						{modalState
+							? <Modal
+								modalState={modalState}
+								images={phoneDetails.phone_images}
+								name={`${phoneDetails.brand} ${phoneDetails.phone_name}`}
+							/>
+							: null
+						}
+					</>
+					: <p>{"Loading"}</p>
+				}
+			</main>
+			<Footer />
+		</>
 	);
 }
 
