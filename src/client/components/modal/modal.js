789f1a9e2e7ed444fa1dc4f1ee001d1830022bc9
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2022-12-28 16:58:12 +0100

    [WIP][feat] Add the carousel features

--- src/client/components/modal/modal.js
+++ src/client/components/modal/modal.js
@@ -1,15 +1,49 @@
 import React from "react";
 
+import Button from "../button/button";
+
 const Modal = ({modalState, images, name}) => {
+	const previousImage = (click) => {
+		click.preventDefault();
+	};
+
+	const closeModal = () => {};
+
+	const nextImage = (click) => {
+		click.preventDefault();
+
+		// const 
+	};
+
 	return(
 		<div className={
 			modalState
 			? "modal"
 			: "modal--closed"
 		}>
-			{images.map(image =>
-				<img src={image} alt={name} key={image} />
+			<Button
+				buttonType={"button"}
+				buttonClick={previousImage}
+				buttonClass={"button__previous"}
+			></Button>
+			{images.map((image, index) =>
+				<img
+					key={image}
+					src={image}
+					alt={name}
+					className={`modal__image image${index}`}
+				/>
 			)}
+			<Button
+				buttonType={"button"}
+				buttonClick={closeModal}
+				buttonClass={"button__close"}
+			></Button>
+			<Button
+				buttonType={"button"}
+				buttonClick={nextImage}
+				buttonClass={"button__next"}
+			></Button>
 		</div>
 	);
 }
