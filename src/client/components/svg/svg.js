c5313b1e718fdd8f6a90e1611f98943bcf7dabd6
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2022-12-29 12:37:26 +0100

    [refactor] Remove width and height

--- src/client/components/svg/svg.js
+++ src/client/components/svg/svg.js
@@ -1,10 +1,8 @@
 import React from "react";
 
-const SVG = ({width, height, viewbox, svgClass, children}) => {
+const SVG = ({viewbox, svgClass, children}) => {
 	return(
 		<svg
-			width={width}
-			height={height}
 			viewBox={viewbox}
 			xmlns="http://www.w3.org/2000/svg"
 			className={svgClass && svgClass}
