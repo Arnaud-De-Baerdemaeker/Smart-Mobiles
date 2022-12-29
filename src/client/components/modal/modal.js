e0cb14c16c431c5405bbaa7c02ea593fc804c357
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2022-12-29 12:57:20 +0100

    [feat] Set the modal
    - Set condition to block scroll on body when the modal is open, and remove blocking when close
    - Add the SVG for the button

--- src/client/components/modal/modal.js
+++ src/client/components/modal/modal.js
@@ -1,19 +1,19 @@
 import React from "react";
 
 import Button from "../button/button";
+import SVG from "../svg/svg";
 
-const Modal = ({modalState, images, name}) => {
-	const previousImage = (click) => {
-		click.preventDefault();
-	};
+const Modal = ({modalState, setModalState, images, name}) => {
+	const closeModal = () => {
+		setModalState(false);
+	}
 
-	const closeModal = () => {};
-
-	const nextImage = (click) => {
-		click.preventDefault();
-
-		// const 
-	};
+	if(modalState === true) {
+		document.querySelector("body").classList.add("scroll--blocked");
+	}
+	else {
+		document.querySelector("body").classList.remove("scroll--blocked");
+	}
 
 	return(
 		<div className={
@@ -21,29 +21,31 @@ const Modal = ({modalState, images, name}) => {
 			? "modal"
 			: "modal--closed"
 		}>
-			<Button
-				buttonType={"button"}
-				buttonClick={previousImage}
-				buttonClass={"button__previous"}
-			></Button>
-			{images.map((image, index) =>
-				<img
-					key={image}
-					src={image}
-					alt={name}
-					className={`modal__image image${index}`}
-				/>
-			)}
+			<div className={"modal__gallery"}>
+				{images.map((image, index) =>
+					<img
+						key={image}
+						src={image}
+						alt={name}
+						className={`modal__image image${index + 1}`}
+					/>
+				)}
+			</div>
 			<Button
 				buttonType={"button"}
 				buttonClick={closeModal}
 				buttonClass={"button__close"}
-			></Button>
-			<Button
-				buttonType={"button"}
-				buttonClick={nextImage}
-				buttonClass={"button__next"}
-			></Button>
+			>
+				<SVG
+					viewbox={"-7 -7 50 50"}
+					class={"svg__close"}
+				>
+					<path
+						d="M7.5 6C6 4.49998 4.5 6 6 7.5L16.5 18L6.00004 28.5C4.50003 30 6.00007 31.5 7.50006 30L18 19.5L28.5 30C30 31.5 31.5 30 30 28.5L19.5 18L30 7.5C31.5 5.99997 30 4.49998 28.5 5.99998L18 16.5L7.5 6Z"
+						className={"svg__close"}
+					/>
+				</SVG>
+			</Button>
 		</div>
 	);
 }
