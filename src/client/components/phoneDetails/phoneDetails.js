2fca2cade1afb9c465327c9cd3f8ab3f67b7a702
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2022-12-30 13:10:04 +0100

    [refactor] Handle the modals for gallery and fullscreen displays
    - On click, the data of the clicked image are stored in a state and sent to the fullscreen modal
    - Set functions to control the displays of modals

--- src/client/components/phoneDetails/phoneDetails.js
+++ src/client/components/phoneDetails/phoneDetails.js
@@ -2,10 +2,25 @@ import React, {useState} from "react";
 
 import Header from "../header/header";
 import Modal from "../modal/modal";
+import Button from "../button/button";
+import SVG from "../svg/svg";
 import Footer from "../footer/footer";
 
 const PhoneDetails = ({phoneDetails}) => {
-	const [modalState, setModalState] = useState(false);
+	const [galleryModalState, setGalleryModalState] = useState(false);
+	const [fullscreenModalState, setFullscreenModalState] = useState(false);
+	const [clickedImage, setClickedImage] = useState({
+		src: null,
+		alt: null
+	});
+
+	const openGalleryModal = () => {
+		setGalleryModalState(true);
+	};
+
+	const closeGalleryModal = () => {
+		setGalleryModalState(false);
+	};
 
 	const transformTitle = (title) => {
 		let splitWord = title.split("");
@@ -21,7 +36,19 @@ const PhoneDetails = ({phoneDetails}) => {
 		});
 
 		return <h3 className={"container__title"}>{letters}</h3>;
-	}
+	};
+
+	const openFullscreenModal = (click) => {
+		setFullscreenModalState(true);
+		setClickedImage({
+			src: click.target.src,
+			alt: click.target.alt
+		});
+	};
+
+	const closeFullscreenModal = () => {
+		setFullscreenModalState(false);
+	};
 
 	return(
 		<>
@@ -33,7 +60,7 @@ const PhoneDetails = ({phoneDetails}) => {
 						<div className={"phoneDetails__presentation"}>
 							<div className={"presentation"}>
 								<div
-									onClick={() => setModalState(!modalState)}
+									onClick={openGalleryModal}
 									className={"presentation__thumbnailContainer"}
 								>
 									<img
@@ -88,11 +115,67 @@ const PhoneDetails = ({phoneDetails}) => {
 							)}
 						</div>
 						<Modal
-							modalState={modalState}
-							setModalState={setModalState}
-							images={phoneDetails.phone_images}
-							name={`${phoneDetails.brand} ${phoneDetails.phone_name}`}
-						/>
+							galleryModalState={galleryModalState}
+							setGalleryModalState={setGalleryModalState}
+							modalClass={
+								galleryModalState
+								? "modal__gallery"
+								: "modal__gallery--closed"
+							}
+						>
+							<div className={"gallery"}>
+								{phoneDetails.phone_images.map((image, index) =>
+									<img
+										key={image}
+										src={image}
+										alt={`${phoneDetails.brand} ${phoneDetails.phone_name}`}
+										onClick={openFullscreenModal}
+										className={`gallery__image image${index + 1}`}
+									/>
+								)}
+							</div>
+							<Button
+								buttonType={"button"}
+								buttonClick={closeGalleryModal}
+								buttonClass={"button__close"}
+							>
+								<SVG
+									viewbox={"-7 -7 50 50"}
+									class={"svg__close"}
+								>
+									<path
+										d="M7.5 6C6 4.49998 4.5 6 6 7.5L16.5 18L6.00004 28.5C4.50003 30 6.00007 31.5 7.50006 30L18 19.5L28.5 30C30 31.5 31.5 30 30 28.5L19.5 18L30 7.5C31.5 5.99997 30 4.49998 28.5 5.99998L18 16.5L7.5 6Z"
+										className={"svg__close"}
+									/>
+								</SVG>
+							</Button>
+						</Modal>
+						<Modal
+							modalClass={
+								fullscreenModalState
+								? "modal__fullscreen"
+								: "modal__fullscreen--closed"
+							}
+						>
+							<div className={"fullscreen"}>
+								<img src={clickedImage.src} alt={clickedImage.alt} className={"fullscreen__image"} />
+							</div>
+							<Button
+								buttonType={"button"}
+								buttonClick={closeFullscreenModal}
+								buttonClass={"button__close"}
+							>
+								<SVG
+									viewbox={"-7 -7 50 50"}
+									class={"svg__close"}
+								>
+									<path
+										d="M7.5 6C6 4.49998 4.5 6 6 7.5L16.5 18L6.00004 28.5C4.50003 30 6.00007 31.5 7.50006 30L18 19.5L28.5 30C30 31.5 31.5 30 30 28.5L19.5 18L30 7.5C31.5 5.99997 30 4.49998 28.5 5.99998L18 16.5L7.5 6Z"
+										className={"svg__close"}
+									/>
+								</SVG>
+							</Button>
+						</Modal>
 					</>
 					: <p>{"Loading"}</p>
 				}
