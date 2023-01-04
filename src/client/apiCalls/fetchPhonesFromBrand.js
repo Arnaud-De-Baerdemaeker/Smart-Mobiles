55aec15a35643d1a2962d6ed1bfca18feed6bcd2
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2023-01-04 16:51:14 +0100

    [feat] Modify the call to handle pagination

--- src/client/apiCalls/fetchPhonesFromBrand.js
+++ src/client/apiCalls/fetchPhonesFromBrand.js
@@ -2,16 +2,25 @@ import axios from "axios";
 
 import {baseURL} from "../App";
 
-const getPhonesFromBrand = async (brand) => {
+
+const getPhonesFromBrand = async (brand, page) => {
+	const params = {};
+	if (page) {
+		params.page = page;
+	}
+
 	const request = await axios({
 		method: "GET",
 		url: `${baseURL}/brands/${brand}`,
 		headers: {
 			"content-type": "application/json"
-		}
+		},
+		params: params
 	});
 
+	console.log(request);
+
 	return request;
-}
+};
 
 export {getPhonesFromBrand};
