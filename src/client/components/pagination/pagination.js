61d98370013ce969d892904daccea896888b318f
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2023-01-06 14:03:08 +0100

    [refactor] Set the SVGs for the buttons

--- src/client/components/pagination/pagination.js
+++ src/client/components/pagination/pagination.js
@@ -1,12 +1,13 @@
 import React from "react";
 
 import Button from "../button/button";
+import SVG from "../svg/svg";
 
 import {getPhonesFromBrand} from "../../apiCalls/fetchPhonesFromBrand";
 
 const Pagination = ({selectedBrand, currentPage, setCurrentPage, totalPages, setPhones}) => {
 	return(
-		<div>
+		<div className={"pagination"}>
 			<Button
 				buttonType={"button"}
 				buttonClick={() => {
@@ -21,9 +22,14 @@ const Pagination = ({selectedBrand, currentPage, setCurrentPage, totalPages, set
 						})
 					}
 				}}
-				buttonClass={""}
+				buttonClass={"button__previous"}
 			>
-				{"Previous page"}
+				<SVG
+					viewbox={"-5 -5 60 60"}
+					svgClass={"svg__previous"}
+				>
+					<path d={"M24.2929 31.2929L8 15C7.44772 14.4477 6.55228 14.4477 6 15C5.44772 15.5523 5.44771 16.4477 6 17L24.2929 35.2929C24.6834 35.6834 25.3166 35.6834 25.7071 35.2929L44 17C44.5523 16.4477 44.5523 15.5523 44 15C43.4477 14.4477 42.5523 14.4477 42 15L25.7071 31.2929C25.3166 31.6834 24.6834 31.6834 24.2929 31.2929Z"} />
+				</SVG>
 			</Button>
 			<Button
 				buttonType={"button"}
@@ -39,9 +45,14 @@ const Pagination = ({selectedBrand, currentPage, setCurrentPage, totalPages, set
 						})
 					}
 				}}
-				buttonClass={""}
+				buttonClass={"button__next"}
 			>
-				{"Next page"}
+				<SVG
+					viewbox={"-5 -5 60 60"}
+					svgClass={"svg__next"}
+				>
+					<path d={"M24.2929 31.2929L8 15C7.44772 14.4477 6.55228 14.4477 6 15C5.44772 15.5523 5.44771 16.4477 6 17L24.2929 35.2929C24.6834 35.6834 25.3166 35.6834 25.7071 35.2929L44 17C44.5523 16.4477 44.5523 15.5523 44 15C43.4477 14.4477 42.5523 14.4477 42 15L25.7071 31.2929C25.3166 31.6834 24.6834 31.6834 24.2929 31.2929Z"} />
+				</SVG>
 			</Button>
 		</div>
 	);
