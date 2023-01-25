11d039e7c950a77fd166aa006d1dbae3827b6f32
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2023-01-25 11:41:14 +0100

    [refactor] Add a title to the message

--- src/client/components/failedFetch/failedFetch.js
+++ src/client/components/failedFetch/failedFetch.js
@@ -8,11 +8,14 @@ import React from "react";
 
 const FailedFetch = () => {
 	return(
-		<p className={"failedFetch"}>
-			{"We were unable to respond to your request."}
-			<br />
-			{"Please try again later."}
-		</p>
+		<div className={"failedFetch"}>
+			<h2>{"Something went wrong..."}</h2>
+			<p>
+				{"We were unable to respond to your request."}
+				<br />
+				{"Please try again later."}
+			</p>
+		</div>
 	);
 }
 
