c545d27620f07b3d0182292978776694b574c5d9
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2023-01-25 11:21:58 +0100

    [refactor] Handle the request failed case
    - Display a message in case of the request failed
    - Some code cleaning

--- src/client/components/render/render.js
+++ src/client/components/render/render.js
@@ -10,6 +10,7 @@ import {Link} from "react-router-dom";
 import PhonesList from "../phonesList/phonesList";
 import Pagination from "../pagination/pagination";
 import PhoneCard from "../phoneCard/phoneCard";
+import FailedFetch from "../failedFetch/failedFetch";
 
 const Render = ({
 	latestPhones,
@@ -21,71 +22,54 @@ const Render = ({
 	currentPage,
 	setCurrentPage,
 	totalPages,
-	acquirePhoneDetails
+	acquirePhoneDetails,
+	fetchError
 }) => {
 	if(brandTitle && phones) {
-		document.title = `${brandTitle} | Smartmobiles`;
-		return <PhonesList title={brandTitle}>
-			<Pagination
-				selectedBrandPagination={selectedBrandPagination}
-				setPhones={setPhones}
-				currentPage={currentPage}
-				setCurrentPage={setCurrentPage}
-				totalPages={totalPages}
-			/>
-			{phones.map(phone =>
-				<Link
-					to={`/${phone.slug}`}
-					onClick={() => {
-						acquirePhoneDetails(phone.detail);
-						document.title = `${phone.phone_name} | Smartmobiles`;
-					}}
-					key={phone.slug}
-					className={"phonesList__link"}
-				>
-					<PhoneCard
-						imgSrc={phone.image}
-						imgAlt={`${phone.brand} ${phone.phone_name}`}
-						title={`${phone.brand} ${phone.phone_name}`}
-					/>
-				</Link>
-			)}
-			<Pagination
-				selectedBrandPagination={selectedBrandPagination}
-				setPhones={setPhones}
-				currentPage={currentPage}
-				setCurrentPage={setCurrentPage}
-				totalPages={totalPages}
-			/>
-		</PhonesList>;
+		document.title = `${brandTitle} | Smartmobiles`
+
+		return(
+			<PhonesList title={brandTitle}>
+				<Pagination
+					selectedBrandPagination={selectedBrandPagination}
+					setPhones={setPhones}
+					currentPage={currentPage}
+					setCurrentPage={setCurrentPage}
+					totalPages={totalPages}
+				/>
+				{phones.map(phone =>
+					<Link
+						to={`/${phone.slug}`}
+						onClick={() => {
+							acquirePhoneDetails(phone.detail);
+							document.title = `${phone.phone_name} | Smartmobiles`;
+						}}
+						key={phone.slug}
+						className={"phonesList__link"}
+					>
+						<PhoneCard
+							imgSrc={phone.image}
+							imgAlt={`${phone.brand} ${phone.phone_name}`}
+							title={`${phone.brand} ${phone.phone_name}`}
+						/>
+					</Link>
+				)}
+				<Pagination
+					selectedBrandPagination={selectedBrandPagination}
+					setPhones={setPhones}
+					currentPage={currentPage}
+					setCurrentPage={setCurrentPage}
+					totalPages={totalPages}
+				/>
+			</PhonesList>
+		);
 	}
 	else if(searchResult) {
 		document.title = `${searchResult.title} | Smartmobiles`;
-		return <PhonesList title={searchResult.title}>
-			{searchResult.phones.map(phone =>
-				<Link
-					to={`/${phone.slug}`}
-					onClick={() => {
-						acquirePhoneDetails(phone.detail);
-						document.title = `${phone.phone_name} | Smartmobiles`;
-					}}
-					key={phone.slug}
-					className={"phonesList__link"}
-				>
-					<PhoneCard
-						imgSrc={phone.image}
-						imgAlt={phone.phone_name}
-						title={phone.phone_name}
-					/>
-				</Link>
-			)}
-		</PhonesList>;
-	}
-	else {
-		if(latestPhones) {
-			document.title = `${latestPhones.title} | Smartmobiles`;
-			return <PhonesList title={latestPhones.title}>
-				{latestPhones.phones.map(phone =>
+
+		return(
+			<PhonesList title={searchResult.title}>
+				{searchResult.phones.map(phone =>
 					<Link
 						to={`/${phone.slug}`}
 						onClick={() => {
@@ -102,11 +86,42 @@ const Render = ({
 						/>
 					</Link>
 				)}
-			</PhonesList>;
-		}
-		else {
-			return <div className={"loading"}>{"Loading..."}</div>;
-		}
+			</PhonesList>
+		);
+	}
+	else if(fetchError) {
+		document.title = "Request failed | Smartmobiles";
+
+		return(
+			<PhonesList title={"Something went wrong..."}>
+				<FailedFetch />
+			</PhonesList>
+		);
+	}
+	else if(latestPhones) {
+		document.title = `${latestPhones.title} | Smartmobiles`;
+
+		return(
+			<PhonesList title={latestPhones.title}>
+				{latestPhones.phones.map(phone =>
+					<Link
+					to={`/${phone.slug}`}
+					onClick={() => {
+						acquirePhoneDetails(phone.detail);
+						document.title = `${phone.phone_name} | Smartmobiles`;
+					}}
+						key={phone.slug}
+						className={"phonesList__link"}
+						>
+						<PhoneCard
+							imgSrc={phone.image}
+							imgAlt={phone.phone_name}
+							title={phone.phone_name}
+							/>
+					</Link>
+				)}
+			</PhonesList>
+		);
 	}
 };
 
